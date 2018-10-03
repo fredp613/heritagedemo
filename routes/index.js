f77aed3ad53a5b4f4653378a356aca41ce343ae6
fredp613 <fredp613@gmail.com>
2018-10-02 21:45:23 -0400

    optimizations

--- routes/index.js
+++ routes/index.js
@@ -10,8 +10,29 @@ router.get('/programs', function(req, res, next) {
   res.render('programs', { title: 'Express' });
 });
 
+router.get('/program', function(req, res, next) {
+  res.render('program', { title: 'Express' });
+});
+
+router.get('/application', function(req, res, next) {
+  res.render('application', { title: 'Express' });
+});
+
+router.get('/prospect', function(req, res, next) {
+  res.render('prospect', { title: 'Express' });
+});
+
+router.get('/expressapply', function(req, res, next) {
+  res.render('quickapply', { title: 'Express' });
+});
+
 router.get('/search', function(req, res, next) {
   res.render('search', { title: 'Express' });
 });
+
+router.get('/success', function(req, res, next) {
+  res.render('success', { title: 'Express', layout: false });
+});
+
 
 module.exports = router;
